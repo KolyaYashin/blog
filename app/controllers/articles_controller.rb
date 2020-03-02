106ceb3a64c709422d45b9521f834357354b32e3
KolyaYashin <kol.yashin@yandex.ru>
2020-03-02 22:04:34 +0300

    edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,11 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+
 	def create
   		@article = Article.new(article_params)
 
@@ -23,6 +28,16 @@ class ArticlesController < ApplicationController
   		else
   			render 'new'
   		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 
 
