d7c173c4fc8fc4bc93d557bf6241c3a168e59104
KolyaYashin <kol.yashin@yandex.ru>
2020-03-10 20:40:31 +0300

    migrations1

--- db/migrate/20200310152104_create_products.rb
+++ db/migrate/20200310152104_create_products.rb
@@ -1,8 +1,16 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+    	t.string :name
+    	t.text :description
+    	t.timestamps
 
-      t.timestamps
+    	reversible do |dir|
+    		change_table :products do |t|
+    			dir.up { t.change :price, :string}
+    			dir.down {t.change :price, :integer}
+    		end
+    	end
     end
   end
 end
