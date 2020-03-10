f56f419c8cb6c08c78a4b00cefe28aa384f84880
KolyaYashin <kol.yashin@yandex.ru>
2020-03-10 18:49:11 +0300

    CRUD product

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,14 @@ puts p.name # "Some book"
 self.table_name = "my_products"
 self.primary_id = "product_id"
 
+user = User.create(name: "David", occupation: "Programmer")
+
+users = User.all
+
+david = User.find_by(name: "David")
+david.name = "Dave"
+david.save
+
+User.find_by(name: "Dave").destroy
+
 end
