bc77d8d59211935e602d2cb6299782fa727b62f0
KolyaYashin <kol.yashin@yandex.ru>
2020-03-13 18:54:14 +0300

    few validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
-						length: {minimum: 5}, length: {maximum: 50}
+						length: {in: 5..50}
 	validates_associated :comments
-
+	validates :title, :text, exclusion: { in: %w(www us ca jp),
+    message: "%{value} is reserved." }
+    validates :title, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+    validates :title, uniqueness: true
 end
