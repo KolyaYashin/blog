a940cca73ec3203be5b9108bf0e2e8d1d045cc56
KolyaYashin <kol.yashin@yandex.ru>
2020-03-13 18:31:54 +0300

    errors msg

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
-						length: {minimum: 5}
+						length: {minimum: 5}, length: {maximum: 50}
 	validates_associated :comments
 
 end
